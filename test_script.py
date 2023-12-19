4e68d88d3a6e724cdb75a8a5ab9a5e1bec4e30f2
mohsuhel <127845338+mohsuhel@users.noreply.github.com>
2023-12-19 12:12:48 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,36 +1,26 @@
+import logging
+import requests
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 
-class TestWebsiteLoad(unittest.TestCase):
-    def setUp(self):
-        print("Setting up the test...")
-        # Set up Chrome in headless mode
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')  # Add this line to avoid sandbox issues
-        self.driver = webdriver.Chrome(options=chrome_options)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-    def test_website_load(self):
-        print("Loading the website...")
-        self.driver.get("https://atg.world/")
-
+class TestWebsiteLoader(unittest.TestCase):
+    def test_load_atg_world_website(self):
+        url = "https://atg.world"
+        logger.info(f"Attempting to load the {url} website...")
+        response = None
         try:
-            print("Waiting for an element with class 'landing-signin-btn' to be present...")
-            WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'landing-signin-btn'))
-            )
-            print("Website loaded successfully!")
-        except Exception as e:
-            print(f"Failed to load the website: {e}")
-            self.fail("Website did not load successfully")
-
-    def tearDown(self):
-        print("Tearing down the test...")
-        self.driver.quit()
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+            logger.info(f"Website {url} loaded successfully.")
+        except requests.RequestException as e:
+            logger.error(f"Failed to load the website: {e}")
+        
+        if response is None or not response.ok:
+            logger.error(f"Website loading test failed. URL: {url}")
+            self.fail(f"Website loading test failed. URL: {url}")
 
 if __name__ == "__main__":
     unittest.main()
