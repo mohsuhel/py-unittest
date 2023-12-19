e5fba5849c875d90b92680e24468efc6798761e3
mohsuhel <127845338+mohsuhel@users.noreply.github.com>
2023-12-19 08:54:38 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -3,18 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 class TestWebsiteLoad(unittest.TestCase):
     def setUp(self):
         print("Setting up the test...")
-        self.driver = webdriver.Chrome()
+        # Set up Chrome in headless mode
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(options=chrome_options)
 
     def test_website_load(self):
         print("Loading the website...")
         self.driver.get("https://atg.world/")
 
         try:
-            print("Waiting for an element with ID 'your_element_id' to be present...")
+            print("Waiting for an element with class 'landing-signin-btn' to be present...")
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, 'landing-signin-btn'))
             )
